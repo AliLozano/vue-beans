7881ebfa75259ca0d51ca1eced530061970fb7cf
ali <alilozanoc@gmail.com>
2022-06-01 11:40:53 -0500

    release: v0.1.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.1.7](https://github.com/AliLozano/vue-beans/compare/v0.1.6...v0.1.7) (2022-06-01)
+
+
+
 ## [0.1.6](https://github.com/AliLozano/vue-beans/compare/v0.1.5...v0.1.6) (2022-06-01)
 
 
--- packages/decorators/src/CatchException.ts
+++ packages/decorators/src/CatchException.ts
@@ -1,45 +1,49 @@
-import { Decorator, createInitializerDecorator } from "@vue-beans/beans";
+import { Decorator, createInitializerDecorator } from '@vue-beans/beans'
 
-type ReturnHandler = boolean | unknown;
+type ReturnHandler = boolean | unknown
 
-type HandlerFn<T> = (ex: Error, instance: T) => ReturnHandler;
+type HandlerFn<T> = (ex: Error, instance: T) => ReturnHandler
 
-type HandlerFnGetter<T> = (it: T) => HandlerFn<T>;
+type HandlerFnGetter<T> = (it: T) => HandlerFn<T>
 
 /*
  * Return the method function wrapping with a try catch using handler
  * */
 function catchingWrapper<T>(
-    instance: T,
-    method: (...args: unknown[]) => unknown,
-    handler: HandlerFn<T>,
+  instance: T,
+  method: (...args: unknown[]) => unknown,
+  handler: HandlerFn<T>
 ) {
-    const handleError = (error: Error) => {
-        const dontStopPropagation = handler(error, instance);
-        if (dontStopPropagation === false) throw error;
-        return dontStopPropagation;
-    };
+  const handleError = (error: Error) => {
+    const dontStopPropagation = handler(error, instance)
+    if (dontStopPropagation === false) throw error
+    return dontStopPropagation
+  }
 
-    return (...args: unknown[]) => {
-        try {
-            const result = method.apply(instance, args) as any;
-            if (result && result.then) {
-                return result.catch((error: Error) => {
-                    return handleError(error);
-                });
-            }
-            return result;
-        } catch (e) {
-            return handleError(e);
-        }
-    };
+  return (...args: unknown[]) => {
+    try {
+      const result = method.apply(instance, args) as any
+      if (result && result.then) {
+        return result.catch((error: Error) => {
+          return handleError(error)
+        })
+      }
+      return result
+    } catch (e) {
+      return handleError(e)
+    }
+  }
 }
 
-export function CatchException<T extends Record<keyof T, unknown>>(getHandler: HandlerFnGetter<T>): Decorator<T> {
-    return createInitializerDecorator<T>((instance, constructor, callback, isStatic) => {
-        const callbackOwner = (isStatic ? constructor : instance) as { [key: string]: () => unknown };
-        const fnHandler = getHandler(instance as T);
-        const fnToWrap = callbackOwner[callback] as (...args: unknown[]) => unknown;
-        callbackOwner[callback] = catchingWrapper<T>(instance as T, fnToWrap, fnHandler);
-    });
+type PartialRecord<K extends keyof any, T> = { [P in K]?: T } // allows classes with optional fields.
+
+export function CatchException<T extends PartialRecord<keyof T, unknown>>(
+  getHandler: HandlerFnGetter<T>
+): Decorator<T> {
+  return createInitializerDecorator<T>((instance, constructor, callback, isStatic) => {
+    const callbackOwner = (isStatic ? constructor : instance) as { [key: string]: () => unknown }
+    const fnHandler = getHandler(instance as T)
+    const fnToWrap = callbackOwner[callback] as (...args: unknown[]) => unknown
+    callbackOwner[callback] = catchingWrapper<T>(instance as T, fnToWrap, fnHandler)
+  })
 }
